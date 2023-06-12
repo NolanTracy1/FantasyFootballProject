68f325d7a76d2a647a90149976ed088ab9bd00e5
NolanTracy1 <125767620+NolanTracy1@users.noreply.github.com>
2023-06-11 22:07:01 -0400

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,4 @@
+--Join Best Ball data with player data to get the Teams--
 SELECT dbo.best_ball_finals.*, dbo.player_stats.Tm
 FROM dbo.best_ball_finals LEFT OUTER JOIN dbo.player_stats
 ON dbo.best_ball_finals.player_name = dbo.player_stats.Player
